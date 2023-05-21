6cec8d2cac4faf20cef8624eb30368ab00b9de2f
subframe7536 <1667077010@qq.com>
2023-05-21 12:22:51 +0800

    feat: no longer need to use provider for $store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,5 @@
 import { trackStore } from '@solid-primitives/deep'
-import type { FlowComponent, JSX, ParentComponent, ParentProps } from 'solid-js'
-import { batch, createComponent, createContext, createEffect, createMemo, observable, on, onMount, untrack, useContext } from 'solid-js'
-
+import { batch, createEffect, createMemo, createRoot, getOwner, on, onMount, runWithOwner, untrack } from 'solid-js'
 import { createStore, reconcile, unwrap } from 'solid-js/store'
 import type { SetStoreFunction, Store } from 'solid-js/store/types/store'
 
@@ -9,9 +7,9 @@ export type BaseStore<T, S, R> = R & S & {
   store: T
 }
 export type UseStoreReturn<T, S, R> = BaseStore<T, S, R> & {
-  $patch: (state: T) => void
+  $patch: (state: Partial<T>) => void
   $reset: () => void
-  $subscribe: ReturnType<typeof observable<T>>['subscribe']
+  $subscribe: (callback: (state: T) => void) => void
 }
 
 export type StoreSetup<
@@ -91,32 +89,40 @@ function parseAction<T extends object>(functions: T) {
 }
 
 /**
+ * Creates a deep clone of the given target object or array.
+ * @param target The target object or array to clone.
+ * @returns The deep clone of the target.
+ */
+export function deepClone<T>(target: T): T {
+  const newTarget = (Array.isArray(target) ? [] : {}) as T
+  for (const key in target) {
+    if (typeof target[key] === 'object' && target[key]) {
+      newTarget[key] = deepClone(target[key])
+    } else {
+      newTarget[key] = target[key]
+    }
+  }
+  return newTarget
+}
+
+/**
  * create state
  * @param name store name
- * @param setup store setup function
- * @returns provider and hooks
+ * @param setup store setup object
  */
 export function $store<
-  T extends object = {},
-  Getter extends GetterReturn = {},
-  Action extends ActionReturn = {},
+T extends object = {},
+Getter extends GetterReturn = {},
+Action extends ActionReturn = {},
 >(
   name: string,
   setup: StoreSetup<T, Getter, Action>,
-): readonly [provider: ParentComponent, useStore: () => UseStoreReturn<T, Getter, Action>] {
+): UseStoreReturn<T, Getter, Action> {
   const { action = () => ({}), getter = () => ({}), state, persist } = setup
   const initalState = typeof state === 'function' ? state() : state
-  const [store, setStore] = createStore<T>(initalState, { name })
-  const ctxData = {
-    store,
-    ...parseGetter(getter(store)),
-    ...parseAction(action(setStore)),
-    $patch: (state: T) => setStore(reconcile(state, { key: name, merge: true })),
-    $reset: () => setStore(initalState),
-    $subscribe: observable(() => trackStore(store)).subscribe,
-  } as UseStoreReturn<T, Getter, Action>
+  const [store, setStore] = createStore<T>(deepClone(initalState), { name })
 
-  const setupContext = () => {
+  const storeFn = () => {
     const option = normalizePersistOption(name, persist)
     if (option) {
       const { debug, key, serializer: { deserialize, serialize }, storage } = option
@@ -134,44 +140,24 @@ export function $store<
           debug && console.error(`[$store - ${key}]: ${e}`)
         }
       })
-      createEffect(on(() => trackStore(store), () => {
-        debug && console.log(`[$store - ${key}]: update to ${JSON.stringify(store)}`)
-        storage.setItem(option.key, serialize(unwrap(store)))
+      createEffect(on(() => trackStore(store), (state) => {
+        debug && console.log(`[$store - ${key}]: update to ${JSON.stringify(state)}`)
+        storage.setItem(option.key, serialize(state))
       }, { defer: true }))
     }
-    return ctxData
+    return {
+      store,
+      ...parseGetter(getter(store)),
+      ...parseAction(action(setStore)),
+      $patch: (state: Partial<T>) => setStore(reconcile(Object.assign({}, unwrap(store), state), { key: name, merge: true })),
+      $reset: () => setStore(initalState),
+      $subscribe: (callback: (state: T) => void) =>
+        runWithOwner(getOwner(), () => {
+          createEffect(on(() => trackStore(store), (state) => {
+            callback(state)
+          }, { defer: true }))
+        }),
+    } as UseStoreReturn<T, Getter, Action>
   }
-  const ctx = createContext<UseStoreReturn<T, Getter, Action>>(ctxData, { name: `ctx_${name}` })
-
-  return [
-    (props: ParentProps): JSX.Element =>
-      createComponent(ctx.Provider, {
-        value: setupContext(),
-        get children() {
-          return props.children
-        },
-      }),
-    () => useContext(ctx),
-  ] as const
-}
-export function $Providers(props: {
-  values: readonly FlowComponent[]
-  children: JSX.Element
-}): JSX.Element {
-  const { values } = props
-  const fn = (i: number): JSX.Element => {
-    const item = values[i]
-
-    if (!item) {
-      return props.children
-    }
-
-    const ctxProps: { value?: any; children: JSX.Element } = {
-      get children() {
-        return fn(i + 1)
-      },
-    }
-    return createComponent(item, ctxProps)
-  }
-  return fn(0)!
+  return createRoot(storeFn)
 }
--- test/store.test.tsx
+++ test/store.test.tsx
@@ -1,8 +1,6 @@
-import { createRoot } from 'solid-js'
 import { cleanup, fireEvent, render } from '@solidjs/testing-library'
-import { describe, expect, test } from 'vitest'
-import { $Providers, $store } from '../src'
-import { normalizePersistOption } from '../src/store'
+import { describe, expect, test, vi } from 'vitest'
+import { $store, normalizePersistOption } from '../src/store'
 
 describe('test normalizePersistOption()', () => {
   test('returns undefined with undefined option', () => {
@@ -76,65 +74,72 @@ describe('test normalizePersistOption()', () => {
 })
 describe('test store', () => {
   test('$store()', () => {
-    const kv = new Map()
-    createRoot(() => {
-      const [, useTestStore] = $store('test', {
-        state: { test: 1 },
-        getter: state => ({
-          doubleValue() {
-            return state.test * 2
-          },
-        }),
-        action: set => ({
-          double() {
-            set('test', test => test * 2)
-          },
-          plus(num: number) {
-            set('test', test => test + num)
-          },
-        }),
-        persist: {
-          enable: true,
-          storage: {
-            getItem(key) {
-              return kv.get(key)
-            },
-            setItem(key, value) {
-              kv.set(key, value)
-            },
-          },
-          debug: true,
+    const useTestStore = $store('test', {
+      state: { test: 1, foo: 'bar' },
+      getter: state => ({
+        doubleValue() {
+          return state.test * 2
         },
-      })
-      const { store, double, plus, doubleValue } = useTestStore()
-      expect(store.test).toBe(1)
-      expect(doubleValue()).toBe(2)
-      double()
-      expect(store.test).toBe(2)
-      expect(doubleValue()).toBe(4)
-      plus(200)
-      expect(store.test).toBe(202)
-      expect(doubleValue()).toBe(404)
+      }),
+      action: set => ({
+        double() {
+          set('test', test => test * 2)
+        },
+        plus(num: number) {
+          set('test', test => test + num)
+        },
+      }),
     })
+    const callback = vi.fn()
+    const { store, double, plus, doubleValue, $patch, $reset, $subscribe } = useTestStore
+    $subscribe(callback)
+    expect(store.test).toBe(1)
+    expect(doubleValue()).toBe(2)
+    double()
+    expect(store.test).toBe(2)
+    expect(doubleValue()).toBe(4)
+    plus(200)
+    expect(store.test).toBe(202)
+    expect(doubleValue()).toBe(404)
+    $patch({ foo: 'baz' })
+    expect(store.foo).toBe('baz')
+    $reset()
+    expect(store.test).toBe(1)
+    expect(store.foo).toBe('bar')
+    expect(doubleValue()).toBe(2)
+    expect(callback).toHaveBeenCalledTimes(4)
   })
-  test('should create a Solid store provider and hook', () => {
+  test('should successfully use nest $store()', () => {
     const initialState = { count: 0 }
-    const [Provider, useStore] = $store('test', {
+    const useStore = $store('test', {
       state: initialState,
+      getter: store => ({
+        fresh: () => {
+          return store.count * 2 + 20
+        },
+      }),
       action: set => ({
         increment: () => set('count', n => n + 1),
         decrement: () => set('count', n => n - 1),
       }),
     })
-    const { store, decrement, increment } = useStore()
+    const { store, fresh, decrement, increment } = useStore
+    const useTempStore = $store('temp', {
+      state: initialState,
+      action: set => ({
+        generate: () => {
+          increment()
+          set('count', fresh())
+        },
+      }),
+    })
+    const { store: tempStore, generate } = useTempStore
     const { unmount, getByTestId } = render(() => (
-      <$Providers values={[Provider]}>
-        <div>
-          <p data-testid="value">{store.count}</p>
-          <button data-testid="increment" onClick={increment}>Increment</button>
-          <button data-testid="decrement" onClick={decrement}>Decrement</button>
-        </div>
-      </$Providers>
+      <div>
+        <p data-testid="value">{store.count}</p>
+        <button data-testid="increment" onClick={increment}>Increment</button>
+        <button data-testid="decrement" onClick={decrement}>Decrement</button>
+      </div>
     ))
 
     const p = getByTestId('value')
@@ -145,13 +150,15 @@ describe('test store', () => {
     expect(p.innerText).toBe('1')
     fireEvent.click(decrementBtn)
     expect(p.innerText).toBe('0')
+    generate()
+    expect(tempStore.count).toBe(22)
     unmount()
   })
 
   test('should persist state to storage', () => {
     const initialState = { count: 0 }
     const kv = new Map()
-    const [Provider, useStore] = $store('test', {
+    const useStore = $store('test', {
       state: initialState,
       action: set => ({
         increment: () => set('count', n => n + 1),
@@ -170,15 +177,13 @@ describe('test store', () => {
         debug: true,
       },
     })
-    const { store, decrement, increment } = useStore()
+    const { store, decrement, increment } = useStore
     const { unmount, getByTestId } = render(() => (
-      <Provider>
-        <div>
-          <p data-testid="value">{store.count}</p>
-          <button data-testid="increment" onClick={increment}>Increment</button>
-          <button data-testid="decrement" onClick={decrement}>Decrement</button>
-        </div>
-      </Provider>
+      <div>
+        <p data-testid="value">{store.count}</p>
+        <button data-testid="increment" onClick={increment}>Increment</button>
+        <button data-testid="decrement" onClick={decrement}>Decrement</button>
+      </div>
     ))
 
     const p = getByTestId('value')
@@ -196,9 +201,7 @@ describe('test store', () => {
     fireEvent.click(incrementBtn)
     unmount()
     const { container: newContainer } = render(() => (
-      <Provider>
-        <p>{useStore().store.count}</p>
-      </Provider>
+      <p>{useStore.store.count}</p>
     ))
     const newP = newContainer.querySelector('p')!
     expect(kv.get('test')).toBe('{"count":2}')
